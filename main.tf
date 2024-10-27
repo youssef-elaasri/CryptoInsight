cb79c31df072dbc2eb5899714a329bd814686586
dehbiy <yakoub.dehbi@grenoble-ino.org>
2024-10-27 12:24:12 +0100

    WIP(untested): First implementation of the terraform file Still haven't tested it

---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.51.0"
+    }
+  }
+}
+
+variable "project_id" {
+  description = "The ID of the project in Google Cloud"
+  type        = string
+}
+
+provider "google" {
+  project = var.project_id
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
+
