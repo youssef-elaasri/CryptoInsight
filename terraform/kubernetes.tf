682251a965d4e394a75ee31c0e24848770916e4c
dehbiy <yakoub.dehbi@grenoble-ino.org>
2024-11-27 16:54:40 +0100

    Feat: Terraform script can run GKE cluster and pods Mofifed the github action to deploy the cluster with terraform This commit is meant to test it, there may come other commits after if this one ends up failing

--- terraform/kubernetes.tf
+++ terraform/kubernetes.tf
@@ -8,10 +8,33 @@
 #   depends_on = [google_container_cluster.primary]
 # }
 
-resource "kubernetes_manifest" "test_pod" {
-  manifest = yamldecode(file("${path.module}/../Database/test.yaml"))
-  depends_on = [
-    google_container_cluster.primary,
-    data.google_client_config.default
-  ]
+resource "kubernetes_manifest" "backend_deployment" {
+  depends_on = [google_container_cluster.primary]
+  manifest   = yamldecode(file("../backend/backend-deployment.yaml"))
+}
+
+resource "kubernetes_manifest" "backend_service" {
+  depends_on = [google_container_cluster.primary]
+  manifest   = yamldecode(file("../backend/backend-service.yaml"))
+}
+
+resource "kubernetes_manifest" "influxdb_service" {
+  depends_on = [google_container_cluster.primary]
+  manifest   = yamldecode(file("../database/influxdb-service.yaml"))
+}
+
+resource "kubernetes_manifest" "influxdb_deployment" {
+  depends_on = [google_container_cluster.primary]
+  manifest   = yamldecode(file("../database/influxdb-deployment.yaml"))
+}
+
+
+resource "kubernetes_manifest" "kafka_deployment" {
+  depends_on = [google_container_cluster.primary]
+  manifest   = yamldecode(file("../database/kafka-deployment.yaml"))
+}
+
+resource "kubernetes_manifest" "kafka_service" {
+  depends_on = [google_container_cluster.primary]
+  manifest   = yamldecode(file("../database/kafka-service.yaml"))
 }
--- terraform/main.tf
+++ terraform/main.tf
@@ -25,13 +25,19 @@ resource "google_compute_network" "vpc_network" {
 }
 
 resource "google_container_cluster" "primary" {
-  name               = "crypto-insights-cluster"
-  location           = var.region
-  initial_node_count = 1
+  name                = "crypto-insights-cluster"
+  location            = var.region
+  initial_node_count  = 1
   deletion_protection = false
 
   workload_identity_config {
     workload_pool = "${var.project_id}.svc.id.goog"
+  }
+
+  node_config {
+    machine_type = "e2-medium"
+    disk_size_gb = 50
+    oauth_scopes = ["https://www.googleapis.com/auth/cloud-platform"]
   }
 
   network = google_compute_network.vpc_network.name
@@ -42,10 +48,10 @@ provider "kubernetes" {
   host                   = "https://${google_container_cluster.primary.endpoint}"
   cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth[0].cluster_ca_certificate)
   token                  = data.google_client_config.default.access_token
-  
+
   # Add these lines for better authentication handling
-  client_certificate     = base64decode(google_container_cluster.primary.master_auth[0].client_certificate)
-  client_key            = base64decode(google_container_cluster.primary.master_auth[0].client_key)
+  client_certificate = base64decode(google_container_cluster.primary.master_auth[0].client_certificate)
+  client_key         = base64decode(google_container_cluster.primary.master_auth[0].client_key)
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
     command     = "gcloud"
@@ -61,3 +67,5 @@ provider "kubernetes" {
     ]
   }
 }
+
+
