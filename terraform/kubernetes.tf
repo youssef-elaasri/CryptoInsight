63aa3a5e133d305d4f4c32a670e10e9c1f36ddad
Jihad <jihadhammoud_@hotmail.com>
2024-11-28 19:20:12 +0100

    deploying sre to cloud

--- terraform/kubernetes.tf
+++ terraform/kubernetes.tf
@@ -38,3 +38,66 @@ resource "kubernetes_manifest" "kafka_service" {
   depends_on = [google_container_cluster.primary]
   manifest   = yamldecode(file("../database/kafka-service.yaml"))
 }
+
+
+resource "kubernetes_manifest" "datastreamer_deployment" {
+  depends_on = [google_container_cluster.primary]
+  manifest   = yamldecode(file("../dataStreamer/datastreamer-deployment.yaml"))
+}
+
+resource "kubernetes_manifest" "datastreamer_svc" {
+  depends_on = [google_container_cluster.primary]
+  manifest   = yamldecode(file("../dataStreamer/datastreamer-svc.yaml"))
+}
+
+
+resource "kubernetes_manifest" "sre_service_account" {
+  depends_on = [google_container_cluster.primary]
+  manifest   = yamldecode(file("../sre/service-account.yaml"))
+}
+
+resource "kubernetes_manifest" "sre_config_map" {
+  depends_on = [google_container_cluster.primary]
+  manifest   = yamldecode(file("../sre/config-map.yaml"))
+}
+
+resource "kubernetes_manifest" "sre_grafana_config_map" {
+  depends_on = [google_container_cluster.primary]
+  manifest   = yamldecode(file("../sre/grafana-datasource-config.yaml"))
+}
+
+resource "kubernetes_manifest" "sre_cluster_role_ks" {
+  depends_on = [google_container_cluster.primary]
+  manifest   = yamldecode(file("../sre/cluster-role-ks.yaml"))
+}
+
+resource "kubernetes_manifest" "sre_cluster_role_ks_binding" {
+  depends_on = [google_container_cluster.primary]
+  manifest   = yamldecode(file("../sre/cluster-role-ks-binding.yaml"))
+}
+
+resource "kubernetes_manifest" "sre_cluster_role_prom" {
+  depends_on = [google_container_cluster.primary]
+  manifest   = yamldecode(file("../sre/cluster-role-prom.yaml"))
+}
+
+resource "kubernetes_manifest" "sre_cluster_role_prom_binding" {
+  depends_on = [google_container_cluster.primary]
+  manifest   = yamldecode(file("../sre/cluster-role-prom-binding.yaml"))
+}
+
+resource "kubernetes_manifest" "sre_deployment" {
+  depends_on = [google_container_cluster.primary]
+  manifest   = yamldecode(file("../sre/sre-deployment.yaml"))
+}
+resource "kubernetes_manifest" "grafana_svc" {
+  depends_on = [google_container_cluster.primary]
+  manifest   = yamldecode(file("../sre/grafana-svc.yaml"))
+}
+resource "kubernetes_manifest" "prom_svc" {
+  depends_on = [google_container_cluster.primary]
+  manifest   = yamldecode(file("../sre/prom-svc.yaml"))
+}
+
+
+
--- terraform/main.tf
+++ terraform/main.tf
@@ -35,7 +35,7 @@ resource "google_container_cluster" "primary" {
   }
 
   node_config {
-    machine_type = "e2-medium"
+    machine_type = "e2-standard-2"
     disk_size_gb = 50
     oauth_scopes = ["https://www.googleapis.com/auth/cloud-platform"]
   }
